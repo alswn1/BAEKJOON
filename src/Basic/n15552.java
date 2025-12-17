8276383adf0910a24f7f3b0192d6035fae1e18bf
mimm <alswn5790@naver.com>
2025-12-17 22:12:46 +0900

    No.15552 solved with "Java" _251217_

---
+++ src/Basic/n15552.java
@@ -0,0 +1,21 @@
+package Basic;
+
+import java.util.*;
+import java.io.*;
+
+public class n15552 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int cnt = Integer.parseInt(br.readLine());
+        for (int i = 0; i < cnt; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            bw.write(a + b + "\n");
+        }
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
