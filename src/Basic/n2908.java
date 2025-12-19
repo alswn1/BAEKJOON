b4f1b343e292fd5b4106c79dc16be9c00a5b8d7a
mimm <alswn5790@naver.com>
2025-12-19 13:52:14 +0900

    No.2908 solved with "Java" _251219_

--- src/Basic/n2908.java
+++ src/Basic/n2908.java
@@ -7,11 +7,8 @@ public class n2908 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        String A = st.nextToken();
-        String B = st.nextToken();
-
-        A = "" + A.charAt(2) + A.charAt(1) + A.charAt(0);
-        B = "" + B.charAt(2) + B.charAt(1) + B.charAt(0);
+        String A = new StringBuilder(st.nextToken()).reverse().toString();
+        String B = new StringBuilder(st.nextToken()).reverse().toString();
 
         if (Integer.parseInt(A) > Integer.parseInt(B)) System.out.println(A);
         else System.out.println(B);
