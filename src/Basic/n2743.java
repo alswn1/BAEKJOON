fc03f3014a4e76ed06dbac0c88482f73b30e63f9
mimm <alswn5790@naver.com>
2025-12-19 13:34:32 +0900

    No.2743 solved with "Java" _251219_

---
+++ src/Basic/n2743.java
@@ -0,0 +1,12 @@
+package Basic;
+
+import java.util.*;
+import java.io.*;
+
+public class n2743 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        System.out.println(str.length());
+    }
+}
