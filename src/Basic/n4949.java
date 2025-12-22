1fbebe1556db20e76a2ce1694e6c967b891ba598
mimm <alswn5790@naver.com>
2025-12-22 17:27:43 +0900

    No.4949 solved with "Java" _251222_

---
+++ src/Basic/n4949.java
@@ -0,0 +1,36 @@
+package Basic;
+
+import java.util.*;
+import java.io.*;
+
+public class n4949 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str;
+        StringBuilder sb = new StringBuilder();
+        while(!(str = br.readLine()).equals(".")) {
+            str = str.replaceAll("[a-zA-Z ]", "");
+            ArrayDeque<Character> st = new ArrayDeque<>();
+            boolean isBalanced = true;
+
+            for (char c : str.toCharArray()) {
+                if (c == '(' || c == '[') st.push(c);
+                else if (c == ')') {
+                    if (st.isEmpty() || st.pop() != '(') {
+                        isBalanced = false;
+                        break;
+                    }
+                }else if (c == ']') {
+                    if (st.isEmpty() || st.pop() != '[') {
+                        isBalanced = false;
+                        break;
+                    }
+                }
+            }
+
+            if (isBalanced && st.isEmpty()) sb.append("yes\n");
+            else sb.append("no\n");
+        }
+        System.out.print(sb);
+    }
+}
