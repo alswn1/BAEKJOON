bf1ddeb28824afd8bf93c69fa1a0727e15499ae8
mimm <alswn5790@naver.com>
2025-12-28 16:39:43 +0900

    No.16236 solved with "Java" _251228_

--- src/Simulation/n16236.java
+++ src/Simulation/n16236.java
@@ -6,76 +6,70 @@ import java.io.*;
 public class n16236 {
     static int N;
     static int[][] map;
-    static int sharkSize = 2;   // 아기 상어 사이즈 (초기값 : 2)
-    static int eatCount = 2;    // 먹어야 하는 물고기 개수 (크기 증가)
-    static int sharkX, sharkY;     // 아기 상어 시작 위치
-    static int time = 0;    // 총 먹는 시간
-    static int[] dx = {-1, 0, 1, 0};
-    static int[] dy = {0, 1, 0, -1};
+    static int[] dx = {0, -1, 1, 0};
+    static int[] dy = {-1, 0, 0, 1};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         map = new int[N][N];
+        int[] cur = null;
 
         for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
                 if (map[i][j] == 9) {
-                    start = new int[] {i, j};
+                    cur = new int[] {i, j};
+                    map[i][j] = 0;
                 }
             }
         }
 
-        find();
-        System.out.println(time);
-    }
+        int size = 2;
+        int eat = 0;
+        int move = 0;
 
-    // 먹을 수 있는 물고기를 찾는 함수
-    static void find() {
         while(true) {
-            fish = new ArrayList<>();
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) {
-                    if (map[i][j] >= 1 && map[i][j] <=8 && map[i][j] < sharkSize) {
-                        fish.add(new int[] {i, j});
-                    }
+            PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> {
+                if (o1[2] != o2[2]) return Integer.compare(o1[2], o2[2]);
+                if (o1[0] != o2[0]) return Integer.compare(o1[0], o2[0]);
+                return Integer.compare(o1[1], o2[1]);
+            });
+            boolean[][] visit = new boolean[N][N];
+
+            pq.add(new int[] {cur[0], cur[1], 0});  // y좌표, x좌표, 거리
+            visit[cur[0]][cur[1]] = true;
+
+            boolean check = false;
+
+            while(!pq.isEmpty()) {
+                cur = pq.poll();
+
+                if (map[cur[0]][cur[1]] != 0 && map[cur[0]][cur[1]] < size) {
+                    map[cur[0]][cur[1]] = 0;
+                    eat++;
+                    move += cur[2];
+                    check = true;
+                    break;
+                }
+
+                for (int i = 0; i < 4; i++) {
+                    int nx = cur[1] + dx[i];
+                    int ny = cur[0] + dy[i];
+
+                    if (nx < 0 || nx >= N || ny < 0 || ny >= N || visit[ny][nx] || map[ny][nx] > size) continue;
+                    pq.add(new int[] {ny, nx, cur[2] + 1});
+                    visit[ny][nx] = true;
                 }
             }
-            int fishCount = fish.size();
+            if (!check) break;
 
-            // 더이상 먹을 수 있는 물고기가 없다면 return
-            if (fishCount == 0) return;
-
-            // 초기값을 1번째 물고기로 세팅 (1마리라면 그대로 진행)
-            int[] eat = fish.get(0);
-            int minLen = Integer.MAX_VALUE;
-            if (fishCount > 1) {
-                for (int i = 0; i < fishCount; i++) {
-                    int len = Math.abs(fish.get(i)[0] - start[0]) + Math.abs(fish.get(i)[1] - start[1]);
-                    if (len < minLen) {
-                        minLen = len;
-                        eat = fish.get(i);
-                    }
-                }
+            if (size == eat) {
+                size++;
+                eat = 0;
             }
-            time += Math.abs(eat[0] - start[0]) + Math.abs(eat[1] - start[1]);
-            map[start[0]][start[1]] = 0;
-            map[eat[0]][eat[1]] = 9;
-            start = eat;
-            eatCount--;
-            if (eatCount == 0) {
-                sharkSize++;
-                eatCount = sharkSize;
-            }
-
         }
-
-
-    }
-
-    static void count() {
-
+        System.out.println(move);
     }
 }
